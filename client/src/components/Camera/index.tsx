957242b553de31b6bf8e38814cb240d3d013ba68
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-12 21:14:17 +0300

    Groundwork for the Camera component

---
+++ client/src/components/Camera/index.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Camera extends Component <ICamera, State> {
+  render() {
+    return (
+      <div>Camera</div>
+    );
+  }
+}
+
+export default Camera;
