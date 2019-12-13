d50c78610bb6c8063b9013839615ef70d62d15f3
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-13 10:11:06 +0300

    Store image in S3

--- server/src/lib/AWS.ts
+++
@@ -1,8 +0,0 @@
-import AWS from 'aws-sdk';
-
-// Set the AWS region and Access Key
-AWS.config.update({ region: 'us-west-1' });
-AWS.config.accessKeyId = process.env.AWS_ACCESS_KEY_ID!;
-
-// Create an AWS SQS service object
-export const sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
---
+++ server/src/lib/ImageStore.ts
@@ -0,0 +1,25 @@
+import AWS, { AWSError } from 'aws-sdk';
+
+const s3 = new AWS.S3();
+
+const save = (name: string, data: File) => {
+  return new Promise((resolve, reject) => {
+    const params = {
+      Bucket: 'camera-challenge-ronnie',
+      Key: `camera/${name}.pdf`,
+      Body: Buffer.from(data, 'base64'),
+      ContentEncoding: 'base64',
+      ContentType: 'image/pdf'
+    }
+
+    s3.putObject(params, (err: AWSError, data: AWS.S3.PutObjectAclOutput) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve(`//camera-challenge-ronnie.s3.amazonaws.com/${params.Key}`)
+      }
+    });
+  })
+}
+
+export default save;
