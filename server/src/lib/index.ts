7fb3bd761c4256461d0ba291c4afb8c43298ca04
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-13 11:35:18 +0300

    Cleaning up

--- server/src/lib/index.ts
+++ server/src/lib/index.ts
@@ -2,7 +2,7 @@ import * as ImageStore from './ImageStore';
 import * as ImageSend from './ImageSend';
 import * as Image from './Image';
 import * as AWS from './AWS';
-import * as Api from './Api';
+import Api from './Api';
 
 export {
   ImageStore,
