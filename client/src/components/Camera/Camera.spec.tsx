7d01b689525bbff11a317d209f96ae1df03588d0
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-14 12:15:59 +0300

    Test - Camera shoul invoke componentDidMount when mounted

--- client/src/components/Camera/Camera.spec.tsx
+++ client/src/components/Camera/Camera.spec.tsx
@@ -5,6 +5,11 @@ import { Camera } from '..';
 import { matchSnapshot } from '../../matchSnapshot';
 
 describe('Camera', () => {
+
+  let spy;
+
+  // afterEach(() => spy.mockClear());
+
   it('should render', () => {
     const tree = shallow(<Camera onCaptureImageSuccess={() => {}} />);
     matchSnapshot(tree);
@@ -12,9 +17,16 @@ describe('Camera', () => {
 
   it('should render video element so one can take a photo', () => {
     const subject = shallow(<Camera onCaptureImageSuccess={() => {}} />);
-    matchSnapshot(subject);
     expect(subject.html()).toContain(
       '<div class=\"camera-container\"><video autoplay=\"\" style=\"width:500px;height:300px;margin-bottom:20px\"></video><button class=\"button \"><span>Take Photo</span></button></div>',
     );
   });
+
+  it('should invoke componentDidMount when mounted', () => {
+    jest.spyOn(Camera.prototype, 'componentDidMount');
+    shallow(<Camera />);
+    expect(Camera.prototype.componentDidMount).toHaveBeenCalled();
+    Camera.prototype.componentDidMount.mockRestore();
+  });
+
 });
