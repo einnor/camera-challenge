18cce5e1712d031bfa4787c021466d5855d31235
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-14 11:49:56 +0300

    Test - Camera should render video element so one can take a photo

--- client/src/components/Camera/Camera.spec.tsx
+++ client/src/components/Camera/Camera.spec.tsx
@@ -9,4 +9,12 @@ describe('Camera', () => {
     const tree = shallow(<Camera onCaptureImageSuccess={() => {}} />);
     matchSnapshot(tree);
   });
+
+  it('should render video element so one can take a photo', () => {
+    const subject = shallow(<Camera onCaptureImageSuccess={() => {}} />);
+    matchSnapshot(subject);
+    expect(subject.html()).toContain(
+      '<div class=\"camera-container\"><video autoplay=\"\" style=\"width:500px;height:300px;margin-bottom:20px\"></video><button class=\"button \"><span>Take Photo</span></button></div>',
+    );
+  });
 });
