262102dc5f6fb28a17c344b55ab12ea61ed41494
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-14 11:26:38 +0300

    Test - Button should emit correct action upon click

--- client/src/components/Button/Buttons.spec.tsx
+++ client/src/components/Button/Buttons.spec.tsx
@@ -4,8 +4,15 @@ import { shallow } from 'enzyme';
 import { Button } from '..';
 
 describe('Button', () => {
-  it('contains the correct text', () => {
+  it('should contain the correct text', () => {
     const subject = shallow(<Button onClick={() => {}} text='Hello world!' />);
     expect(subject.text()).toEqual('Hello world!');
+  });
+
+  it('should emit correct action upon click', () => {
+    let pass = false;
+    const subject = shallow(<Button onClick={() => { pass = true; }} />);
+    subject.find('button').simulate('click');
+    expect(pass).toEqual(true);
   });
 });
